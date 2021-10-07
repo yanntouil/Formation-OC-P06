9fcf9b8e144b7ca0d00c25c558d5f7ecf811cf1e
Yann Touil <yanntouil@gmail.com>
2021-10-07 19:44:45 +0200

    Move api folder

--- public/assets/js/controller/photographer.js
+++ public/assets/js/controller/photographer.js
@@ -73,7 +73,7 @@ export default {
      * Api url
      * @prop {string>} api 
      */
-    api: '../../api/FishEyeData.json',
+    api: '../api/FishEyeData.json',
 
     /**
      * Mount controller
